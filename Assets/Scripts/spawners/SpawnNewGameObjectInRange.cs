c51c6a5b206ed5b900e6c8a44d7f3d27e8f59be9
AndreyGrigorevskiy <74354746+AndreyGrigorevskiy@users.noreply.github.com>
2022-04-05 23:15:54 +0700

    0.0.0.4
    Добавил спавн в тереугольнике( поворачиваю прямоугольник с мпомошью матричного умножения)

--- Assets/Scripts/spawners/SpawnNewGameObjectInRange.cs
+++ Assets/Scripts/spawners/SpawnNewGameObjectInRange.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public enum FormToSpawn
 {
     circle,
-    square
+    square,
+    triangle
 }
 
 public class SpawnNewGameObjectInRange : MonoBehaviour
@@ -20,49 +21,28 @@ public class SpawnNewGameObjectInRange : MonoBehaviour
 
     private void Start()
     {
-
-        if(currentForm == FormToSpawn.circle)
-        {
-            StartCoroutine(spawmEnamyPerTimeCirlce());
-        }
-        else
-        {
-            StartCoroutine(spawmEnamyPerTimeSquare());
-        }
-
+            StartCoroutine(spawmEnamyPerTime());
     }
 
-    private IEnumerator spawmEnamyPerTimeCirlce()
-    {
-        while(keepSpawning)
-        {
-            
-            yield return new WaitForSeconds(timeBetweenSpawn);
-
-            var agle = Random.Range(-180.0f, 180.0f);
-            var inRangePosition = Random.Range(RangeIner, RangeOuter);
-            var ZOnCircle = Mathf.Sin(agle);
-            var XOnCircle = Mathf.Cos(agle);
-
-            Vector3 newPosition1;
-            newPosition1.x = transform.position.x + (XOnCircle * inRangePosition);
-            newPosition1.z = transform.position.z + (ZOnCircle * inRangePosition);
-            newPosition1.y = transform.position.y;
-
-            Instantiate(EntityToSpawn, newPosition1, Quaternion.identity);
-
-        }
-    }
-
-    private IEnumerator spawmEnamyPerTimeSquare()
+    private IEnumerator spawmEnamyPerTime()
     {
         while (keepSpawning)
         {
 
             yield return new WaitForSeconds(timeBetweenSpawn);
-          
 
-            Instantiate(EntityToSpawn, getRandomPositionOnSqare(), Quaternion.identity);
+            switch(currentForm)
+            {
+                case FormToSpawn.circle     :
+                    Instantiate(EntityToSpawn, getRandomPositionOnCircle(), Quaternion.identity);
+                    break;
+                case FormToSpawn.square     :
+                    Instantiate(EntityToSpawn, getRandomPositionOnSqare(), Quaternion.identity);
+                    break;
+                case FormToSpawn.triangle   :
+                    Instantiate(EntityToSpawn, getRandomPositionOnTriangle(), Quaternion.identity);
+                    break;
+            }
 
         }
     }
@@ -75,7 +55,6 @@ public class SpawnNewGameObjectInRange : MonoBehaviour
 
         float rand1;
         float rand2;
-
         rand1 = Random.Range(-RangeOuter, RangeOuter);
         rand2 = Random.Range(RangeIner, RangeOuter);
 
@@ -95,8 +74,54 @@ public class SpawnNewGameObjectInRange : MonoBehaviour
         }
 
         Vector3 newPosition;
-        newPosition.x = randomX;
-        newPosition.z = randomZ;
+        newPosition.x = transform.position.x + randomX;
+        newPosition.z = transform.position.z + randomZ;
+        newPosition.y = transform.position.y;
+
+        return newPosition;
+    }
+
+    private Vector3 getRandomPositionOnCircle()
+    {
+        var agle = Random.Range(-180.0f, 180.0f);
+        var inRangePosition = Random.Range(RangeOuter, RangeOuter);
+        var ZOnCircle = Mathf.Sin(agle);
+        var XOnCircle = Mathf.Cos(agle);
+
+        Vector3 newPosition;
+        newPosition.x = transform.position.x + (XOnCircle * inRangePosition);
+        newPosition.z = transform.position.z + (ZOnCircle * inRangePosition);
+        newPosition.y = transform.position.y;
+
+        return newPosition;
+    }
+    private Vector3 getRandomPositionOnTriangle()
+    {
+        float randomZ;
+        float randomX;
+
+        randomX = Random.Range(-RangeOuter, RangeOuter);
+        randomZ = Random.Range(RangeIner, RangeOuter);
+
+        var angle = 90 * Random.Range(0, 3);
+        float[,] A = new float[2, 2] { { Mathf.Cos(angle), Mathf.Sin(angle) }, { -Mathf.Sin(angle), Mathf.Cos(angle)} };
+        float[] B = new float[2] { randomX, randomZ };
+        float[] C = new float[2] { 0, 0};
+
+        for (int i = 0; i<2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                C[i] += A[i, j] * B[j];
+            }
+        }
+
+        randomX = C[0];
+        randomZ = C[1];
+
+        Vector3 newPosition;
+        newPosition.x = transform.position.x + randomX;
+        newPosition.z = transform.position.z + randomZ;
         newPosition.y = transform.position.y;
 
         return newPosition;
